525f3e1d87e92a6d680e543a01573b7002e81712
ahmed <ahmednas_alani@yahoo.com>
2022-09-01 10:09:27 -0700

    create js file and aad caltip function

---
+++ tips.js
@@ -0,0 +1,6 @@
+const caltip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2 ;
+const bills = [125, 555, 44];
+const tips = [caltip(bills[0]), caltip(bills[1]), caltip(bills[1])] ;
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
